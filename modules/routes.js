feb6ef4e705053fd1221a27ec63c9077b89b429f
Ekaterina Dontsova <ekaterina.dontsova@gmail.com>
2018-02-28 17:33:48 -0800

    addPicture modal

--- modules/routes.js
+++ modules/routes.js
@@ -3,7 +3,7 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 	app.use(express.static(path.join(__dirname, "../public")));
 
 	function checkAuth(req, res, next){
-		var permitRequiredUrls = ["/my", "/add", "/delete", "/like", "/favorites"];
+		var permitRequiredUrls = ["/pins", "/add", "/delete", "/like", "/favorites"];
 		if (permitRequiredUrls.indexOf(req.url) > -1  && (!req.session || !req.session.authenticated)) {
 			if(req.xhr){ // ajax request
 				res.status(400).send({"message" : "You must be logged in for this action", "redirect" : "login"});
@@ -37,6 +37,15 @@ module.exports = function (express, app, path, bcrypt, dbClient) {
 				}
 			});
 		}
+    });
+
+    app.get("/pins", /*checkAuth,*/ function (req, res, next) {
+        	res.render("pins", {});
+    });
+
+    app.post("/add", /*checkAuth,*/ function (req, res, next) {
+            console.log(111);
+            res.status(200).send({"message" : "You succesfully added picture.", "redirect" : "pins"});
     });
 
    	app.get("*", function(req, res){
